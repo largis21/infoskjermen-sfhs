5540909ef210fa2106a8d0cdd1cd7e34e51c8ccd
Lars-Ivar Giskegjerde <lars-ivar.giskegjerde@twoday.com>
2025-08-22 18:37:34 +0200

    Correct trigger

--- src/functions/test.ts
+++ src/functions/test.ts
@@ -1,27 +1,11 @@
 import { AzureOpenAI } from "openai";
-import {
-  DefaultAzureCredential,
-  getBearerTokenProvider,
-} from "@azure/identity";
-import {
-  app,
-  HttpRequest,
-  HttpResponseInit,
-  InvocationContext,
-} from "@azure/functions";
+import { app } from "@azure/functions";
 import { parseICS } from "node-ical";
 import { BlobServiceClient } from "@azure/storage-blob";
 
-const endpoint = process.env.AZURE_OPENAI_ENDPOINT || "";
-
-const credential = new DefaultAzureCredential();
-const apiVersion = "2024-10-21";
-const scope = "https://cognitiveservices.azure.com/.default";
-const azureADTokenProvider = getBearerTokenProvider(credential, scope);
-
-const client = new AzureOpenAI({
-  endpoint,
-  apiVersion,
+const openAIClient = new AzureOpenAI({
+  endpoint: process.env.AZURE_OPENAI_ENDPOINT || "",
+  apiVersion: "2024-10-21",
   // azureADTokenProvider,
   apiKey: process.env.AZURE_OPENAI_API_KEY || "",
 });
@@ -30,10 +14,7 @@ const blobServiceClient = BlobServiceClient.fromConnectionString(
   process.env.AZURE_STORAGE_CONNECTION_STRING || ""
 );
 
-export async function test(
-  request: HttpRequest,
-  context: InvocationContext
-): Promise<HttpResponseInit> {
+export async function handler() {
   const infoskjermenHtml = await fetch(
     "https://app.pintomind.com/remote/boards/info25/posts"
   ).then((res) => res.text());
@@ -43,7 +24,7 @@ export async function test(
   let maxRetries = 3;
 
   while (!validICS && i < maxRetries) {
-    const response = await client.chat.completions
+    const response = await openAIClient.chat.completions
       .create({
         messages: [
           {
@@ -62,10 +43,9 @@ export async function test(
     if (JSON.stringify(events) !== "{}") {
       validICS = response;
     }
+
     i++;
   }
-
-  const blob = new Blob([validICS], { type: "text/calendar" });
 
   const containerName = "calendar";
   const blobName = "cal.ics";
@@ -87,8 +67,7 @@ export async function test(
   return { body: validICS };
 }
 
-app.http("test", {
-  methods: ["GET", "POST"],
-  authLevel: "anonymous",
-  handler: test,
+app.timer("infoskjermenTrigger", {
+  schedule: "0 * * * *",
+  handler,
 });
